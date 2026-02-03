da5fd09319cd78ff4875e9226d8a175eaf624103
DavidjFilho <david_curso@hotmail.com>
2026-02-03 17:32:10 -0300

    criando componente Button

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
 import XIcon from "./assets/icons/x.svg?react";
 import Icon from "./components/icon";
+import Badge from "./components/badge";
+import Button from "./components/button";
 
 export default function App() {
   return (
@@ -25,6 +27,13 @@ export default function App() {
         <Icon svg={SpinnerIcon} animate />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
+      </div>
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
+      </div>
+      <div>
+        <Button icon={PlusIcon}> Nova Tafera</Button>
       </div>
     </div>
   );
